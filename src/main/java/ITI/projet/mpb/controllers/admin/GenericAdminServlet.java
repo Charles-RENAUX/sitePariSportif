7a349eb076d73cafdb8783e32deca88f00934849
Charles RENAUX <charles.renaux@hei.yncrea.fr>
2020-11-17 09:35:17 +0100

    MAJ Correctif AdminServelet car pb path pour le css

--- src/main/java/ITI/projet/mpb/controllers/admin/GenericAdminServlet.java
+++ src/main/java/ITI/projet/mpb/controllers/admin/GenericAdminServlet.java
@@ -2,6 +2,7 @@ package ITI.projet.mpb.controllers.admin;
 
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import javax.servlet.ServletContext;
@@ -13,6 +14,7 @@ public abstract class GenericAdminServlet extends HttpServlet {
         ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
         templateResolver.setPrefix("/WEB-INF/templates/admin/");
         templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
 
         TemplateEngine templateEngine = new TemplateEngine();
         templateEngine.setTemplateResolver(templateResolver);
